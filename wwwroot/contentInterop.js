30deb2e323008d60344e2829095ec23cb67fe442
Dmitrii Dorogoi <dmitrii_dorogoi@epam.com>
2024-09-25 17:41:25 +0200

    wip

--- wwwroot/contentInterop.js
+++ wwwroot/contentInterop.js
@@ -1,11 +1,11 @@
-﻿function executeContentScript(textToType) {
+﻿function executeContentScript(selector, textToType) {
     console.log('Attempting to send message to content script');
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.scripting.executeScript({
             target: { tabId: tabs[0].id },
             files: ['injectedScript.js']
         }, () => {
-            chrome.tabs.sendMessage(tabs[0].id, { action: 'updateText', text: textToType });
+            chrome.tabs.sendMessage(tabs[0].id, { action: 'updateText', selector: selector, text: textToType });
             console.log('Message sent to content script');
         });
     });
--- wwwroot/injectedScript.js
+++ wwwroot/injectedScript.js
@@ -1,10 +1,12 @@
 ﻿chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === 'updateText') {
-        const editableDiv = document.querySelector('div#prompt-textarea[contenteditable="true"]');
+        const editableDiv = document.querySelector(message.selector);
         if (editableDiv) {
             editableDiv.innerHTML = message.text;
             const event = new Event('input', { bubbles: true });
             editableDiv.dispatchEvent(event);
+        } else {
+            console.warn(`Element with selector '${message.selector}' not found.`);
         }
     }
 });
