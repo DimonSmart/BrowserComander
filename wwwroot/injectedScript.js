1df0f7eb4488b0b781e93e5bc9a7439b37137135
Dmitrii Dorogoi <dmitrii_dorogoi@epam.com>
2024-09-27 14:50:55 +0200

    FIX WIP

--- wwwroot/injectedScript.js
+++ wwwroot/injectedScript.js
@@ -1,23 +1,20 @@
-﻿chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === 'updateText') {
-        const editableDiv = document.querySelector(message.selector);
-        if (editableDiv) {
-            editableDiv.innerHTML = message.text;
-            const event = new Event('input', { bubbles: true });
-            editableDiv.dispatchEvent(event);
-        } else {
-            console.warn(`Element with selector '${message.selector}' not found.`);
-        }
-    } else if (message.action === 'getText') {
-        const editableDiv = document.querySelector(message.selector);
-        if (editableDiv) {
-            // Retrun empty string if element found and text is empty
-            const text = editableDiv.innerText || '';
-            sendResponse({ text: text });
-        } else {
-            // Return null if element not found
-            sendResponse({ text: null });
-        }
+﻿if (message.action === 'updateText') {
+    const editableDiv = document.querySelector(message.selector);
+    if (editableDiv) {
+        editableDiv.innerHTML = message.text;
+        const event = new Event('input', { bubbles: true });
+        editableDiv.dispatchEvent(event);
+        sendResponse({ success: true });
+    } else {
+        console.warn(`Element with selector '${message.selector}' not found.`);
+        sendResponse({ success: false });
     }
-    return true;
-});
+} else if (message.action === 'getText') {
+    const editableDiv = document.querySelector(message.selector);
+    if (editableDiv) {
+        const text = editableDiv.innerText || '';
+        sendResponse({ text: text });
+    } else {
+        sendResponse({ text: null });
+    }
+}
