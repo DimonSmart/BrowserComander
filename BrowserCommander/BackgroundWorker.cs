0751f85c534147567ce36b5c63e8f6bd499d22b1
Dmitrii Dorogoi <dmitrii_dorogoi@epam.com>
2024-10-03 18:27:35 +0200

    WIP

--- BrowserCommander/BackgroundWorker.cs
+++ BrowserCommander/BackgroundWorker.cs
@@ -4,6 +4,8 @@ using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR.Client;
 using System;
+using WebExtensions.Net.Runtime;
+using System.Linq;
 
 namespace BrowserCommander
 {
@@ -18,17 +20,30 @@ namespace BrowserCommander
         [Inject]
         private ILogger<BackgroundWorker> Logger { get; set; }
 
+        [Inject]
+        private JSInteropService JSInteropService { get; set; }
+
+
+
         public override void OnInitialized()
         {
             var serverUrl = ServerConfig.ServerAddress;
             Logger.LogInformation($"Server url: {serverUrl}");
             Logger.LogInformation($"HubConnection: {(HubConnection != null)}");
+            Logger.LogInformation($"JSInteropService: {(JSInteropService != null)}");
         }
 
         [BackgroundWorkerMain]
         public override void Main()
         {
             WebExtensions.Runtime.OnInstalled.AddListener(OnInstalled);
+            WebExtensions.Runtime.OnMessage.AddListener(OnContentScriptMessageReceived);
+        }
+
+        private bool OnContentScriptMessageReceived(object arg1, MessageSender sender, Action<object> action)
+        {
+            Logger.LogInformation($"OnContentScriptMessageReceived: {arg1}");
+            return true;
         }
 
         private async Task StartHubConnectionAsync()
@@ -41,10 +56,21 @@ namespace BrowserCommander
                     await HandleCommandAsync(command);
                 });
 
-                HubConnection.On<string>("ReceiveTime", (currentTime) =>
+                HubConnection.On<string>("ReceiveTime", async (currentTime) =>
                 {
                     Logger.LogInformation($"Current server time: {currentTime}");
-                    Console.WriteLine($"Current server time: {currentTime}");
+
+                    try
+                    {
+                        var selector = "div#prompt-textarea[contenteditable=\"true\"]";
+                        var result = await JSInteropService.GetTextAsync(selector);
+                        Logger.LogInformation($"Field:{result}");
+
+                    }
+                    catch (Exception exception)
+                    {
+                        Logger.LogInformation($"Exception: {exception}");
+                    }
                 });
 
                 await HubConnection.StartAsync();
@@ -55,6 +81,17 @@ namespace BrowserCommander
                 Logger.LogError($"Error starting SignalR connection: {ex.Message}");
             }
         }
+
+        private async Task SendMessageToContentScriptAsync(object message)
+        {
+            Logger.LogError($"SendMessageToContentScriptAsync: {message}");
+            var tabs = (await WebExtensions.Tabs.Query(new WebExtensions.Net.Tabs.QueryInfo() { Active = true, CurrentWindow = true })).ToList();
+            if (tabs.Count > 0)
+            {
+                await WebExtensions.Tabs.SendMessage(tabs[0].Id.Value, message);
+            }
+        }
+
 
         private async Task HandleCommandAsync(string command)
         {
